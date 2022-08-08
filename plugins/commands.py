3a706f4d56ffabaab5568064acc435bc1c087027
Oxyver5600 <89983756+Oxyver5600@users.noreply.github.com>
2022-08-08 09:37:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -94,7 +94,7 @@ async def start(bot, message):
                         f_caption=CUSTOM_FILE_CAPTION.format(file_name=title, file_size=size, file_caption=f_caption)
                     except Exception as e:
                         print(e)
-                        f_caption=f_caption
+                        f_caption = f"`{files.file_name}`"
                 if f_caption is None:
                     f_caption = f"{files.file_name}"
                 buttons = [
